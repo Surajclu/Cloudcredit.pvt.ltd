f8bff6847200f926a61e55c0571042127a0f1781
Suraj Gawade <surajgawade217@gmail.com>
2025-01-31 12:39:06 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("contact-form");
-
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        alert("Thank you! Your message has been sent.");
-        form.reset();
-    });
-});
