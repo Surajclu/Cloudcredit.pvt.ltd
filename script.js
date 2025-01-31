740e77706d9c516f1400aa0a3f4afee2598b3e3f
Suraj Gawade <surajgawade217@gmail.com>
2025-01-31 12:27:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("contact-form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        alert("Thank you! Your message has been sent.");
+        form.reset();
+    });
+});
